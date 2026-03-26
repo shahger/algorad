6962d765ee4bd61f1a57bc1a816fd8fba7ceed5f
shakh <shahgoer@gmail.com>
2026-03-26 22:16:29 +0500

    assignment 1 (input updated)

--- src/Main.java
+++ src/Main.java
@@ -3,48 +3,66 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Task 1
-        System.out.println("Task 1:");
-        task1.printDigits(5481);
-
-        // Task 2
-        System.out.println("\nTask 2:");
-        int[] arr = {3, 2, 4, 1};
-        double avg = (double) task2.sum(arr, arr.length) / arr.length;
-        System.out.println(avg);
-
-        // Task 3
-        System.out.println("\nTask 3:");
-        System.out.println(task3.isPrime(7, 2) ? "Prime" : "Composite");
-
-        // Task 4
-        System.out.println("\nTask 4:");
-        System.out.println(task4.factorial(5));
-
-        // Task 5
-        System.out.println("\nTask 5:");
-        System.out.println(task5.fibonacci(5));
-
-        // Task 6
-        System.out.println("\nTask 6:");
-        System.out.println(task6.power(2, 10));
-     // Task 7
-        System.out.println("\nTask 7:");
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt(); // вводим количество
-        task7.reversePrint(n, sc);
+        //Task 1
+        System.out.println("Task 1: Enter a number");
+        int num = sc.nextInt();
+        task1.printDigits(num);
 
-        // Task 8
-        System.out.println("\nTask 8:");
-        System.out.println(task8.onlyDigits("123456", 0) ? "Yes" : "No");
+        //Task 2
+        System.out.println("\nTask 2: Enter size of array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
 
-        // Task 9
-        System.out.println("\nTask 9:");
-        System.out.println(task9.length("hello"));
+        System.out.println("Enter elements:");
+        task2.inputArray(arr, 0, sc);
 
-        // Task 10
-        System.out.println("\nTask 10:");
-        System.out.println(task10.gcd(32, 48));
+        double avg = (double) task2.sum(arr, n) / n;
+        System.out.println("Average: " + avg);
+
+        //Task 3
+        System.out.println("\nTask 3: Enter number");
+        int primeNum = sc.nextInt();
+        System.out.println(task3.isPrime(primeNum, 2) ? "Prime" : "Composite");
+
+        //Task 4
+        System.out.println("\nTask 4: Enter n");
+        int fact = sc.nextInt();
+        System.out.println("Factorial: " + task4.factorial(fact));
+
+        //Task 5
+        System.out.println("\nTask 5: Enter n");
+        int fib = sc.nextInt();
+        System.out.println("Fibonacci: " + task5.fibonacci(fib));
+
+        //Task 6
+        System.out.println("\nTask 6: Enter a and n");
+        int a = sc.nextInt();
+        int pow = sc.nextInt();
+        System.out.println("Result: " + task6.power(a, pow));
+
+        //Task 7
+        System.out.println("\nTask 7: Enter n and then numbers");
+        int count = sc.nextInt();
+        task7.reversePrint(count, sc);
+
+        //Task 8
+        System.out.println("\n\nTask 8: Enter string");
+        String s = sc.next();
+        System.out.println(task8.onlyDigits(s, 0) ? "Yes" : "No");
+
+        //Task 9
+        System.out.println("\nTask 9: Enter string");
+        String str = sc.next();
+        System.out.println("Length: " + task9.length(str));
+
+        //Task 10
+        System.out.println("\nTask 10: Enter two numbers");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println("GCD: " + task10.gcd(x, y));
+
+        sc.close();
     }
 }
--- src/task2.java
+++ src/task2.java
@@ -1,6 +1,13 @@
 public class task2 {
+
     public static int sum(int[] arr, int n) {
         if (n == 0) return 0;
         return arr[n - 1] + sum(arr, n - 1);
+    }
+
+    public static void inputArray(int[] arr, int i, java.util.Scanner sc) {
+        if (i == arr.length) return;
+        arr[i] = sc.nextInt();
+        inputArray(arr, i + 1, sc);
     }
 }
